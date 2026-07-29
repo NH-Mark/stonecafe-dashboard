666c9407d06bbad8d2a4d16c2a05179469d9417d
shirinks <fathimashirin866@gmail.com>
2026-07-29 12:12:13 +0300

    tab view

--- src/features/walk-in/WalkInPOS.tsx
+++ src/features/walk-in/WalkInPOS.tsx
@@ -1,4 +1,3 @@
-// features/walk-in/WalkInPOS.tsx
 "use client";
 
 import { useEffect, useState } from "react";
@@ -8,11 +7,25 @@ import { MenuGrid } from "./components/MenuGrid";
 import { OrderCart } from "./components/OrderCart";
 import { getCategories } from "../menu/category.service";
 import { ModifierDialog } from "./components/modifier-dialog/ModifierDialog";
+import { Button } from "@/components/ui/button";
 
 
 export function WalkInPOS() {
+
     const [categories, setCategories] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+
+    const [selectedCategory, setSelectedCategory] =
+        useState<number | null>(null);
+
+
+    const [showCategories, setShowCategories] =
+        useState(false);
+
+
+    const [showCart, setShowCart] =
+        useState(false);
+
+
 
     useEffect(() => {
 
@@ -20,58 +33,235 @@ export function WalkInPOS() {
 
     }, []);
 
+
+
     async function loadCategories() {
 
-        try {
+        const response = await getCategories();
 
-            const response = await getCategories();
-
-            setCategories(response.data.data);
-
-        } catch (error) {
-
-            console.error(error);
-
-        }
+        setCategories(response.data.data);
 
     }
 
+
+
     return (
-            <div className="flex h-screen flex-col bg-slate-100">
-                <Header />
-                <main className="flex-1 min-h-0 overflow-hidden p-4">
-                    <div
+
+        <div
+            className="
+                flex
+                h-screen
+                flex-col
+                bg-slate-100
+            "
+        >
+
+
+            <Header />
+
+
+
+            <main
+                className="
+                    flex-1
+                    min-h-0
+                    overflow-hidden
+                    p-3
+                "
+            >
+
+
+                <div
+                    className="
+                        grid
+                        h-full
+                        min-h-0
+
+                        lg:grid-cols-12
+                        gap-3
+                    "
+                >
+
+
+
+                    {/* Desktop Categories */}
+
+                    <aside
                         className="
-                            grid
-                            h-full
-                            min-h-0
-                            gap-4
-
-                            grid-cols-1
-
-                            md:grid-cols-7
-
-                            lg:grid-cols-12
+                            hidden
+                            lg:block
+                            lg:col-span-2
                         "
                     >
-                        <aside className="col-span-2 min-h-0">
+
+                        <CategorySidebar
+                            categories={categories}
+                            selectedCategory={selectedCategory}
+                            onSelectCategory={setSelectedCategory}
+                        />
+
+                    </aside>
+
+
+
+                    {/* Menu */}
+
+                    <section
+                        className="
+                            min-h-0
+
+                            lg:col-span-7
+                        "
+                    >
+
+                        <MenuGrid
+                            categoryId={selectedCategory}
+                        />
+
+                    </section>
+
+
+
+                    {/* Desktop Cart */}
+
+                    <aside
+                        className="
+                            hidden
+                            lg:block
+                            lg:col-span-3
+                        "
+                    >
+
+                        <OrderCart />
+
+                    </aside>
+
+
+                </div>
+
+
+            </main>
+
+
+
+            {/* Tablet / Mobile Actions */}
+
+            <div
+                className="
+                    flex
+                    gap-3
+                    border-t
+                    bg-white
+                    p-3
+
+                    lg:hidden
+                "
+            >
+
+                <Button
+                    className="flex-1"
+                    onClick={() =>
+                        setShowCategories(true)
+                    }
+                >
+                    Categories
+                </Button>
+
+
+                <Button
+                    className="flex-1"
+                    onClick={() =>
+                        setShowCart(true)
+                    }
+                >
+                    Order
+                </Button>
+
+            </div>
+
+
+
+            {/* Category Drawer */}
+
+            {
+                showCategories && (
+
+                    <div
+                        className="
+                            fixed
+                            inset-0
+                            z-50
+                            bg-black/30
+                        "
+                    >
+
+                        <div
+                            className="
+                                h-full
+                                w-72
+                                bg-white
+                                p-3
+                            "
+                        >
+
                             <CategorySidebar
                                 categories={categories}
                                 selectedCategory={selectedCategory}
-                                onSelectCategory={setSelectedCategory}
+                                onSelectCategory={
+                                    setSelectedCategory
+                                }
                             />
-                        </aside>
-                        <section className="min-h-0 md:col-span-4 lg:col-span-7">
-                            <MenuGrid
-                                categoryId={selectedCategory}
-                            />
-                        </section>
-                        <aside className="min-h-0 md:col-span-3 lg:col-span-3">
-                            <OrderCart />
-                        </aside>
+
+                        </div>
+
                     </div>
-                </main>
-                <ModifierDialog />
-            </div>
+
+                )
+            }
+
+
+
+            {/* Cart Drawer */}
+
+            {
+                showCart && (
+
+                    <div
+                        className="
+                            fixed
+                            inset-0
+                            z-50
+                            bg-black/30
+                        "
+                    >
+
+                        <div
+                            className="
+                                ml-auto
+                                h-full
+                                w-full
+                                max-w-md
+                                bg-white
+                                p-3
+                            "
+                        >
+
+                            <OrderCart />
+
+                        </div>
+
+                    </div>
+
+                )
+            }
+
+
+
+            <ModifierDialog />
+
+
+        </div>
+
     );
+
 }
