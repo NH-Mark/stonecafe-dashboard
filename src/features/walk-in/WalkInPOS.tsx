1fa9789d14ecd14e4e3a3c7f17f1065d106097d1
shirinks <fathimashirin866@gmail.com>
2026-09-20 10:09:56 +0300

    ipad

--- src/features/walk-in/WalkInPOS.tsx
+++ src/features/walk-in/WalkInPOS.tsx
@@ -284,7 +284,6 @@ export function WalkInPOS() {
                     min-h-0
                     overflow-hidden
                     p-3
-                    pb-0
                 "
             >
                 
